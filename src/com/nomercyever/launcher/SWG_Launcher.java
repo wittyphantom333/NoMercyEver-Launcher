0dbecc557938c1ad45901db3e0fc7adb3af06595
Wittyphantom333 <adamwitt@10.5.10.112>
2018-06-29 18:15:00 -0500

    Uploaded to GitHub

---
+++ src/com/nomercyever/launcher/SWG_Launcher.java
@@ -0,0 +1,20 @@
+
+package com.nomercyever.launcher;
+
+/**
+ *
+ * @author Thomas Otero (H3R3T1C)
+ */
+public class SWG_Launcher {
+
+    public static void main(String[] args) throws Exception {
+           
+        
+        
+           new MainGUI().setVisible(true);
+        
+        
+        
+    }
+
+}
